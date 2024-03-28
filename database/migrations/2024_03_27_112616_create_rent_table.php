057908b5d3c4b8d94da47f2b785ab293b51010ac
alex <345w2011@mail.ru>
2024-03-28 13:08:42 +0400

    Rent

---
+++ database/migrations/2024_03_27_112616_create_rent_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //Schema::create('rent', function (Blueprint $table) {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('building_id');
+            $table->foreign('building_id')->references('id')->on('buildings');
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->foreignId('lead_id');
+            $table->foreign('lead_id')->references('id')->on('leads');
+
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('path', 128);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rent');
+    }
+};
